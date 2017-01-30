d686f1cbb0b95955a284533d3e50a9b69fd7f8aa
Kent Fredric <kentfredric@gmail.com>
2017-01-31 10:42:37 +1300

    Isolate volatile and non-volatile (system-independent) tests

---
+++ t/runtests-volatile.t.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+(
+bash t/cmd/installed-perl-virtuals.t.sh
+bash t/cmd/installed-perl-core.t.sh
+bash t/cmd/installed-deps-perl-subslot-rebuild.t.sh
+bash t/cmd/gen-upgrade-sets.t.sh
+) |& tee t/runtest-volatile.out
+echo "------// DIFF //------------------"
+sed -i 's|[^ ]*/gentoo-perl-helper[.]test[.]gen-upgrade-sets[.][^/]*/|TEMPDIR/|' t/runtest-volatile.out
+diff -Naur t/runtest-volatile.expected t/runtest-volatile.out
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+mv t/runtest-volatile.out t/runtest-volatile.expected
--- t/runtests.t.sh
+++ t/runtests.t.sh
@@ -8,12 +8,8 @@ bash t/bin/interface.t.sh
 bash t/cmd/version.t.sh
 bash t/cmd/list-commands.t.sh
 bash t/cmd/list-commands-desc.t.sh
-bash t/cmd/installed-perl-virtuals.t.sh
 bash t/cmd/print-matching-abi.t.sh
 bash t/cmd/list-excluded-abis.t.sh
-bash t/cmd/installed-perl-core.t.sh
-bash t/cmd/installed-deps-perl-subslot-rebuild.t.sh
-bash t/cmd/gen-upgrade-sets.t.sh
 ) |& tee t/runtest.out
 echo "------// DIFF //------------------"
 sed -i 's|[^ ]*/gentoo-perl-helper[.]test[.]gen-upgrade-sets[.][^/]*/|TEMPDIR/|' t/runtest.out
