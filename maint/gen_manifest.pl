49303cb37f63d81b9253fea30099747a0bbce32e
Kent Fredric <kentfredric@gmail.com>
2017-01-31 13:41:56 +1300

    Add maint/gen_manifest.pl

---
+++ maint/gen_manifest.pl
@@ -0,0 +1,22 @@
+#!perl
+use 5.020;
+use strict;
+use warnings;
+
+
+use Git::Wrapper;
+use Path::Tiny qw( path );
+
+my $git = Git::Wrapper->new('.');
+my $manifest = path( __FILE__ )->sibling('MANIFEST')->openw_raw;
+my $LF = chr(0x0A);
+
+my @out;
+
+for my $i ( $git->ls_files('--exclude-standard', '--full-name') ) {
+  next if $i =~ /\.orig$/;
+  next if $i =~ /^maint\//;
+  next if $i =~ /-volatile/;
+  $manifest->printf("%s${LF}", $i);
+}
+
