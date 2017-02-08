e313ec7ac399a1f0fd373e6f1c3942bfc1a3cdd7
Kent Fredric <kentfredric@gmail.com>
2017-02-08 16:10:09 +1300

    Add installed-deps-atom utility
    changelog: new-command: installed-deps-atom

---
+++ t/cmd/installed-deps-atom.t.sh
@@ -0,0 +1,5 @@
+source lib/core-functions.sh
+
+echo "==== [ bin/gentoo-perl installed-deps-atom app-shells/bash ] ===="
+bash bin/gentoo-perl installed-deps-atom app-shells/bash
+echo
--- t/runtests-volatile.t.sh
+++ t/runtests-volatile.t.sh
@@ -4,6 +4,7 @@ bash t/cmd/installed-perl-virtuals.t.sh
 bash t/cmd/installed-perl-core.t.sh
 bash t/cmd/installed-deps-perl-subslot-rebuild.t.sh
 bash t/cmd/gen-upgrade-sets.t.sh
+bash t/cmd/installed-deps-atom.t.sh
 ) |& tee t/runtest-volatile.out
 echo "------// DIFF //------------------"
 sed -i 's|[^ ]*/gentoo-perl-helper[.]test[.]gen-upgrade-sets[.][^/]*/|TEMPDIR/|' t/runtest-volatile.out
