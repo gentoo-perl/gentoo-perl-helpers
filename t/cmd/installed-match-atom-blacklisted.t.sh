99d87f9099d831aaa896e9c399696e5378afee4d
Kent Fredric <kentfredric@gmail.com>
2017-02-08 21:23:21 +1300

    Add command installed-match-atom-blacklisted
    changelog: new-command: installed-match-atom-blacklisted

---
+++ t/cmd/installed-match-atom-blacklisted.t.sh
@@ -0,0 +1,6 @@
+source lib/core-functions.sh
+
+echo "==== [ bin/gentoo-perl installed-match-atom-blacklisted dev-lang/perl:0/5.22= ] ===="
+bash bin/gentoo-perl installed-match-atom-blacklisted "dev-lang/perl:0/5.22="
+echo
+
